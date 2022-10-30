a82c1c62886af1de055b88bed0185d7c2d11a6a3
bikramregmi <bikram.regmi7@gmail.com>
2022-10-30 09:46:26 +0000

    initial design small changes

--- src/main/java/com/bipuz/BipuzApplication.java
+++ src/main/java/com/bipuz/BipuzApplication.java
@@ -1,16 +1,36 @@
 package com.bipuz;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import javax.inject.Inject;
 
 @SpringBootApplication
 public class BipuzApplication {
+    private static final Logger log = LoggerFactory.getLogger(BipuzApplication.class);
 
-    public static void main(String[] args) {
-        SpringApplication.run(BipuzApplication.class, args);
+    @Inject
+    private Environment env;
+
+    public static void main(String[] args) throws UnknownHostException {
+        final SpringApplication app = new SpringApplication(BipuzApplication.class);
+//        DefaultProfileUtil.addDefaultProfile(app);
+        final Environment env = app.run(args).getEnvironment();
+        log.info(
+                "\n----------------------------------------------------------\n\t" +
+                        "Application '{}' is running! Access URLs:\n\t" +
+                        "Local: \t\thttp://127.0.0.1:{}\n\t" +
+                        "External: \thttp://{}:{}\n----------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                env.getProperty("server.port"),
+                InetAddress.getLocalHost().getHostAddress(),
+                env.getProperty("server.port")
+        );
     }
 
 }
--- src/main/java/com/bipuz/controller/Me.java
+++ src/main/java/com/bipuz/controller/Me.java
@@ -2,8 +2,14 @@ package com.bipuz.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class Me {
@@ -13,10 +19,14 @@ public class Me {
 
         return "index";
     }
-    @ResponseBody
-    @GetMapping("hell")
-    public String hellMe() {
 
-        return "hell yeah";
+
+    @RequestMapping("/navbar")
+    public String go(HttpServletRequest request, HttpServletResponse response) {
+        HttpSession session = request.getSession();
+        ServletContext sc = session.getServletContext();
+        String x = sc.getRealPath("/");
+        System.out.println(x);
+        return "navbar";
     }
 }
